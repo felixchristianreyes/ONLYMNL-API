bc4f0e0a5112cc7a4fa5a9736e4c4ef142bb78ca
Christian <felix.christian.reyes@gmail.com>
2022-08-17 18:21:49 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('markers', [MarkersController::class, 'index']);
-Route::post('/add-markers', [MarkersController::class, 'store']);
+Route::post('add-markers', [MarkersController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
